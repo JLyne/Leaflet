2d651abde1ab00ff9032bc39f3deae50343ea81a
James Lyne <jim+github@not-null.co.uk>
2022-06-25 20:27:26 +0100

    Remove zoom control

--- src/control/index.js
+++ src/control/index.js
@@ -1,11 +1,8 @@
 import {Control, control} from './Control';
 import {Layers, layers} from './Control.Layers';
-import {Zoom, zoom} from './Control.Zoom';
 
 Control.Layers = Layers;
-Control.Zoom = Zoom;
 
 control.layers = layers;
-control.zoom = zoom;
 
 export {Control, control};
